08a045db884363605835bb8b396950aa81a19957
nikdany <4.iamnik@gmail.com>
2017-05-25 16:39:05 +0300

    my commit's message

---
+++ src/week1/hometask1/TestPush2Git.java
@@ -0,0 +1,10 @@
+package week1.hometask1;
+
+/**
+ * Created by 4iamn on 25.05.2017.
+ */
+public class TestPush2Git {
+    public static void main(String[] args) {
+        System.out.println("Yeee boy");
+    }
+}
